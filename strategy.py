97c0501889fab039d27153d1a2f161284bbace57
Boyi Zeng <zengboyi@gmail.com>
2026-03-16 03:37:25 +0000

    hypothesis: fine-tune vol_pctrank (0.40-1.29)

--- strategy.py
+++ strategy.py
@@ -112,7 +112,7 @@ class Strategy:
                 # Vol percentile: size down when vol is historically extreme
                 vpr = extras.get("vol_pctrank_720")
                 if vpr is not None and vpr == vpr:
-                    size *= max(0.42, min(1.28, 1.38 - vpr * 0.77))
+                    size *= max(0.40, min(1.29, 1.39 - vpr * 0.78))
 
                 # Momentum reversal filter
                 mr = extras.get("momentum_reversal_24h")
